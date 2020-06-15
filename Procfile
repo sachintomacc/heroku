90e86c8af5cc0473030da76479718d213da53621
sachin tom <sachintomacc@gmail.com>
2020-06-15 14:15:09 +0530

    second

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn templateproject.wsgi --log-file -
--- templateproject/settings.py
+++ templateproject/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '56n!k6$jy^@@e^5gxyu*tfk^ev9(+58r5fjrbr4rjd^9h05=%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sachintom-ecomm.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticroot')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
